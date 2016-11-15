411e7922fe328c614900add48572c2eeb8e0a278
Will Grauvogel <builtbywill@gmail.com>
2016-11-15 13:27:43 -0500

    add payload tests, re-org test file

--- test/index.js
+++ test/index.js
@@ -6,37 +6,13 @@ const utils = require('../lib/utils');
 const schema = require('../lib/schema');
 const fixtures = require('../lib/fixtures');
 
-function validate(data) {
-	return ajv.validate(schema, utils.createEventStorePayload(data));
-}
-
-function invalidTest(testName, event) {
-	it(testName, function * () {
-		expect(validate([event])).to.be.false;
-	});
-}
-
-function eventTests(name, event) {
-	describe(name + ' event', function () {
-		it('default is valid', function * () {
-			expect(validate([event])).to.be.true;
-		});
-		if (fixtures.invalidEventTestsByName.hasOwnProperty(name)) {
-			describe('invalid tests', function () {
-				const invalidEventTests = fixtures.invalidEventTestsByName[name];
-				for (var testName in invalidEventTests) {
-					if (invalidEventTests.hasOwnProperty(testName)) {
-						invalidTest(testName, invalidEventTests[testName]);
-					}
-				}
-			});
-		}
-	});
-}
+/*
+ * Event Tests
+ */
 
 describe('events', function () {
 	it('all events payload is valid', function * () {
-		expect(validate(fixtures.events)).to.be.true;
+		expect(validateData(fixtures.events, true)).to.be.true;
 	});
 
 	for (var name in fixtures.eventsByName) {
@@ -46,19 +22,18 @@ describe('events', function () {
 	}
 });
 
+/*
+ * Entity Tests
+ */
+
 describe('entities', function () {
 	it('all entities payload is valid', function * () {
-		expect(validate(fixtures.entities)).to.be.true;
+		expect(validateData(fixtures.entities, true)).to.be.true;
 	});
 	for (var i = 0; i < fixtures.entities.length; i++) {
 		const entity = fixtures.entities[i];
 		it(`entity of @type="${entity['@type']}" is valid`, function * () {
-			const valid = validate([entity]);
-			if (!valid) {
-				console.log(JSON.stringify(entity, null, 2));
-				console.log(JSON.stringify(ajv.errors, null, 2));
-			}
-			expect(valid).to.be.true;
+			expect(validateData([entity], true)).to.be.true;
 		});
 	}
 	// for (var i = 0; i < fixtures.invalidEntities.length; i++) {
@@ -73,3 +48,105 @@ describe('entities', function () {
 	// 	});
 	// }
 });
+
+
+/*
+ * EventStore Payload Tests
+ */
+
+describe('event store payload', function() {
+	const payload = utils.createEventStorePayload([fixtures.events[0]]);
+	it('default is valid', function * () {
+		expect(validatePayload(payload, true)).to.be.true;
+	});
+
+	let p = null;
+	it('sensor is required', function * () {
+		p = _.merge({}, payload);
+		delete p.sensor;
+		expect(validatePayload(p, false)).to.be.false;
+	});
+	it('sensor must not be null', function * () {
+		p = _.merge({}, payload);
+		p.sensor = null;
+		expect(validatePayload(p, false)).to.be.false;
+	});
+	it('sensor value must be typeof uri', function * () {
+		p = _.merge({}, payload);
+		p.sensor = 'not a uri value';
+		expect(validatePayload(p, false)).to.be.false;
+	});
+
+	it('sendTime is required', function * () {
+		p = _.merge({}, payload);
+		delete p.sendTime;
+		expect(validatePayload(p, false)).to.be.false;
+	});
+	it('sendTime must not be null', function * () {
+		p = _.merge({}, payload);
+		p.sendTime = null;
+		expect(validatePayload(p, false)).to.be.false;
+	});
+	it('sendTime value must be typeof date', function * () {
+		p = _.merge({}, payload);
+		p.sendTime = 'not a date value';
+		expect(validatePayload(p, false)).to.be.false;
+	});
+
+	it('data is required', function * () {
+		p = _.merge({}, payload);
+		delete p.data;
+		expect(validatePayload(p, false)).to.be.false;
+	});
+	it('data must not be null', function * () {
+		p = _.merge({}, payload);
+		p.data = null;
+		expect(validatePayload(p, false)).to.be.false;
+	});
+	it('data must have at least one item', function * () {
+		p = _.merge({}, payload);
+		p.data = [];
+		expect(validatePayload(p, false)).to.be.false;
+	});
+});
+
+/*
+ * Shared Functions
+ */
+
+function validatePayload(payload, expected) {
+	const valid = ajv.validate(schema, payload);
+	if (valid !== expected) {
+		console.log(JSON.stringify(payload, null, 2));
+		console.log(JSON.stringify(ajv.errors, null, 2));
+	}
+	return valid;
+}
+
+function validateData(data, expected) {
+	return validatePayload(utils.createEventStorePayload(data), expected);
+}
+
+function invalidTest(testName, event) {
+	it(testName, function * () {
+		expect(validateData([event], false)).to.be.false;
+	});
+}
+
+function eventTests(name, event) {
+	describe(name + ' event', function () {
+		it('default is valid', function * () {
+			expect(validateData([event], true)).to.be.true;
+		});
+		if (fixtures.invalidEventTestsByName.hasOwnProperty(name)) {
+			describe('invalid tests', function () {
+				const invalidEventTests = fixtures.invalidEventTestsByName[name];
+				for (var testName in invalidEventTests) {
+					if (invalidEventTests.hasOwnProperty(testName)) {
+						invalidTest(testName, invalidEventTests[testName]);
+					}
+				}
+			});
+		}
+	});
+}
